e5c7264ae9210dc22d7748ccd8bc51d5bba9aae2
Ewsunk <ewsunk99@gmail.com>
2023-10-13 16:35:30 +0200

    Jugador creado

---
+++ GerardMiguel_Furbo/Clases/Jugador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerardMiguel_Furbo.Clases
+{
+    
+
+    internal class Jugador
+    {
+        private string nombre;
+        private int dorsal;
+
+        Jugador()
+        {
+
+        }
+
+    }
+}
