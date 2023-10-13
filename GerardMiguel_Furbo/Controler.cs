3ca3ffe26cdbda5afcf93969619f0a22d92396fb
Ewsunk <ewsunk99@gmail.com>
2023-10-13 17:00:44 +0200

    Menu de controlador en desarrollo

--- GerardMiguel_Furbo/Controler.cs
+++ GerardMiguel_Furbo/Controler.cs
@@ -8,15 +8,61 @@ namespace GerardMiguel_Furbo
 {
     internal class Controler
     {
+        private static List<String> clubes;     // Cambiar a objeto club
+        private static int clubSeleccionado;
+
         public static void Start()
         {
+            clubes = new List<String>();        // Cambiar a objeto club
+            clubSeleccionado = 0;
 
             Menu();
         }
 
         private static void Menu()
         {
+            int eleccion;
 
+            newClub();
+
+            do
+            {
+
+                Console.WriteLine("1- Crear club");
+                Console.WriteLine("2- Seleccionar club");
+                Console.WriteLine("3- Crear equipo");
+                Console.WriteLine("4- Crear jugador");
+                Console.WriteLine("5- Ver equipo");
+                Console.WriteLine("6- Ver Jugadores");
+
+                eleccion = Convert.ToInt32(System.Console.ReadLine());
+
+                switch(eleccion)
+                {
+                    case 1:
+                        NewClub();
+                        break;
+
+                    case 2:
+                        SelectClub();
+                        break;
+                }
+
+
+            } while (eleccion != 0);
+        }
+
+        private static void NewClub()
+        {
+
+        }
+
+        private static void SelectClub()
+        {
+            foreach (var club in clubes)
+            {
+
+            }
         }
 
     }
