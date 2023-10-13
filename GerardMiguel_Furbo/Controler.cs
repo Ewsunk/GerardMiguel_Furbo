74fa012f9343acee24b488569b916a4ad839dad8
Ewsunk <ewsunk99@gmail.com>
2023-10-13 16:31:08 +0200

    Agregar archivos de proyecto.

---
+++ GerardMiguel_Furbo/Controler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerardMiguel_Furbo
+{
+    internal class Controler
+    {
+        public static void Start()
+        {
+
+            Menu();
+        }
+
+        private static void Menu()
+        {
+
+        }
+
+    }
+}
