6a9c666cc440cb23af3955db60921f29c0346f15
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-13 18:51:13 +0200

    Lógica para ganar equipo Partido.

---
+++ GerardMiguel_Furbo/Clases/Partido.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerardMiguel_Furbo.Clases
+{
+    public class Partido
+    {
+        private Equipo equipoA;
+        private Equipo equipoB;
+        private Equipo equipoGanador;
+
+        public Partido(Equipo equipoA, Equipo equipoB) 
+        {
+            this.equipoA = equipoA;
+            this.equipoB = equipoB;
+        }
+
+
+        public Equipo JugarPartido()
+        {
+            var rand = new Random();
+            var numero = rand.Next(2);
+
+            switch (numero)
+            {
+                case 0:
+                    equipoGanador = null;
+                    break;
+
+                case 1:
+                    equipoGanador = equipoA;
+                    break;
+
+                case 2:
+                    equipoGanador = equipoB;
+                    break;
+            }
+
+            return equipoGanador;
+        }
+
+    }
+}
