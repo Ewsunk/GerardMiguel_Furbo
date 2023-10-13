c3717083d9417404a953d8b89f743f3f95c1c674
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-10-13 16:54:22 +0200

    Añadida lógica, getters y setters de Jugador.

--- GerardMiguel_Furbo/Clases/Equipo.cs
+++ GerardMiguel_Furbo/Clases/Equipo.cs
@@ -17,16 +17,11 @@ namespace GerardMiguel_Furbo.Clases
             jugadoresLista = new List<Jugador>();
         }
 
+        //Getters y Setters
         public string Nombre { get { return nombre; } set { nombre = value; } }
+        public List<Jugador> JugadoresLista { get { return jugadoresLista; } set { jugadoresLista = value; } }
 
-        public void AddJugador(Jugador jugador)
-        {
-
-        }
-
-        public void QuitarJugador(Jugador jugador)
-        {
-
-        }
+        public void AddJugador(Jugador jugador) { jugadoresLista.Add(jugador); }
+        public void QuitarJugador(Jugador jugador) { jugadoresLista.Remove(jugador); }
     }
 }
